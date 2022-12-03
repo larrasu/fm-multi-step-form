5edc64defde073a9c58959468e3b28e15f543698
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-03 21:39:47 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      title: "Frontend Mentor | Multi-Step Form",
+      link: [{ rel: "icon", href: "/favicon-32x32.png" }],
+    },
+  },
+  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  googleFonts: {
+    families: {
+      Ubuntu: [400, 500, 700],
+    },
+  },
+  tailwindcss: {},
+});
